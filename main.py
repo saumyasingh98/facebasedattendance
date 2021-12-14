a96899d4ac2344516d1528736c1329cdfdc1d39e
saaz5410 <saumya.singh@northwestern.edu>
2021-12-13 18:46:04 -0600

    commit

--- main.py
+++ main.py
@@ -15,20 +15,20 @@ import numpy as np
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-krish_image = face_recognition.load_image_file("Krish/krish.jpg")
-krish_face_encoding = face_recognition.face_encodings(krish_image)[0]
+sam_image = face_recognition.load_image_file("Images/sam.png")
+sam_face_encoding = face_recognition.face_encodings(sam_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-bradley_image = face_recognition.load_image_file("Bradley/bradley.jpg")
-bradley_face_encoding = face_recognition.face_encodings(bradley_image)[0]
+#bradley_image = face_recognition.load_image_file("Bradley/bradley.jpg")
+#bradley_face_encoding = face_recognition.face_encodings(bradley_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    krish_face_encoding,
-    bradley_face_encoding
+    sam_face_encoding,
+    #bradley_face_encoding
 ]
 known_face_names = [
-    "Krish",
+    "Saumya",
     "Bradley"
 ]
 
